15e045d8d83cee16d4cecc6c449bacd87c169971
Thomas Watson <twatson52@icloud.com>
2025-09-04 13:18:13 +0100

    use per-instance temporary directory
    The temporary names used as preprocessor output are not unique, so if
    another generator instance is running, one can be writing into a file
    while the other is reading it. This usually manifests as an error like
    `<some_file.idl>:1:0: error: mismatched input '<EOF>' expecting <etc>`.
    
    Fix by randomly generating a unique directory, plus being nice and
    cleaning it up if it is unlikely to be useful.
    
    Note that specifying a temporary directory is not actually implemented,
    despite a flag to do so being listed in the help.

--- src/main/java/com/eprosima/microxrcedds/microxrceddsgen.java
+++ src/main/java/com/eprosima/microxrcedds/microxrceddsgen.java
@@ -24,6 +24,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Vector;
 import java.util.jar.Manifest;
 
@@ -61,6 +62,7 @@ public class microxrceddsgen {
     private final String m_defaultOutputDir = "." + File.separator;
     private String m_outputDir = m_defaultOutputDir;
     private String m_tempDir = null;
+    private boolean m_tempDirCleanup = false;
     protected static String m_appName = "microxrceddsgen";
     protected boolean m_test = false;
     protected String m_defaultContainerPreallocSize = "50";
@@ -228,6 +230,13 @@ public class microxrceddsgen {
             }
         }
 
+        if (returnedValue)
+        {
+            // be nice and clean up if no error; user may want files if there
+            // was an issue (though they have no real ability to find them...)
+            cleanUpTemp();
+        }
+
         return returnedValue;
 
     }
@@ -272,16 +281,15 @@ public class microxrceddsgen {
     public boolean globalInit() {
         // Set the temporary folder
         if (m_tempDir == null) {
-            if (m_os.contains("Windows")) {
-                String tempPath = System.getenv("TEMP");
-
-                if (tempPath == null) {
-                    tempPath = System.getenv("TMP");
-                }
-
-                m_tempDir = tempPath;
-            } else if (m_os.contains("Linux") || m_os.contains("Mac")) {
-                m_tempDir = "/tmp/";
+            try
+            {
+                m_tempDir = java.nio.file.Files.createTempDirectory("microxrceddsgen").toString();
+                m_tempDirCleanup = true; // as we created it
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+                return false;
             }
         }
 
@@ -290,6 +298,36 @@ public class microxrceddsgen {
         }
 
         return true;
+    }
+
+    private void cleanUpTemp() {
+        if (!m_tempDirCleanup) {
+            return;
+        }
+
+        try
+        {
+            // https://stackoverflow.com/a/27917071 . it's "very simple"
+            java.nio.file.Path directory = java.nio.file.Paths.get(m_tempDir);
+            java.nio.file.Files.walkFileTree(directory, new java.nio.file.SimpleFileVisitor<java.nio.file.Path>() {
+               @Override
+               public java.nio.file.FileVisitResult visitFile(java.nio.file.Path file, java.nio.file.attribute.BasicFileAttributes attrs) throws IOException {
+                   java.nio.file.Files.delete(file);
+                   return java.nio.file.FileVisitResult.CONTINUE;
+               }
+
+               @Override
+               public java.nio.file.FileVisitResult postVisitDirectory(java.nio.file.Path dir, IOException exc) throws IOException {
+                   java.nio.file.Files.delete(dir);
+                   return java.nio.file.FileVisitResult.CONTINUE;
+               }
+            });
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            // we tried!
+        }
     }
 
     private Project process(String idlFilename) {
